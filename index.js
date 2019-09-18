3dd720b1be7e069bfed1b064462bf2bb3bf8840a
ohk990102 <ohk990102@naver.com>
2019-09-18 23:31:20 +0900

    [Add] Added command-line parsing for better testing

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const StaticEngine = require('./engine.js').StaticEngine
 const fs = require('fs');
+const path = require('path');
 
-var code = fs.readFileSync('./input.js').toString();
+let filepath = '';
 
-console.log(code);
+if(process.argv.length < 3) {
+    filepath = './input.js';
+}
+else {
+    filepath = process.argv[2];
+}
+function walk(dir) {
+    let list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = path.join(dir, file);
+        let stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) { 
+            walk(file);
+        } else if(stat.isFile()){ 
+            let code = fs.readFileSync(file).toString();
+            let engine = new StaticEngine(code);
+            let result = engine.analyze();
+            console.log(file);
+            console.log(result);
+            console.log("");
+        }
+    });
+}
 
-let engine = new StaticEngine(code)
-
-let result = engine.analyze()
-
-//console.log(result.ast.body[0])
-//console.log(result.ast.body[0].body)
-
-//console.log(result.cfg[2][1]);
+try {
+    if(fs.existsSync(filepath)) {
+        let stat = fs.statSync(filepath);
+        if(stat.isFile()) {
+            let code = fs.readFileSync(filepath).toString();
+            let engine = new StaticEngine(code);
+            let result = engine.analyze();
+            console.log(filepath);
+            console.log(result);
+            console.log("");
+        }
+        else if(stat.isDirectory()) 
+            walk(filepath);
+    }
+} catch(err) {
+    console.error(err);
+    process.exit(-1);
+}
