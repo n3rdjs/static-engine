3d2c8f78aad731170b4b59ab8245dbcb32580aca
NerdJS <54803361+n3rdjs@users.noreply.github.com>
2019-09-19 19:33:39 +0900

    Update input.js

--- input.js
+++ input.js
@@ -2,15 +2,35 @@
     a = 4;
 }
 
+obj = { } = new Object();
+obj['apple'] = /mint/.source;
+
 function x(){
     a = 5;
-    {d=5;}
+    {
+        d = 5;
+    }
 }
-b=()=>"hello";
-var a=(m,n)=>{
-    {d=5;}
-    return m+n;
-}
-c=(m,n)=>{
-    return m+n;
+
+this.b = $=>{
+    with (obj) {
+        return apple + [] + $;
+    }
+};
+
+with (this) 
+    with (this) 
+        with (this)
+            this.g = 9;
+
+var a = ((m, n)=>{
+    (function() {
+        'use strict';
+        d = 5;
+    })();
+    return m + n;
+})(a, g) + [];
+
+c = (m, n)=>{
+    return m + n;
 }
