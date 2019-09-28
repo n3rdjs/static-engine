0a063b81affec4bc73b53d8018d6abde4c6dd5d0
time4ruin <time4_ruin@naver.com>
2019-09-28 03:36:00 -0700

    new input binary_ex.js

---
+++ input_folder/binary_ex.js
@@ -0,0 +1,18 @@
+function a(x,y){
+    return x+y;
+}
+var c = 2
+var d = [1, 2, 3];
+var e = {
+    a : 1,
+    b : function(x, y){
+        {
+            let e = [];
+
+        }
+        return x + y;
+    },
+    c : {c1 : 1, c2 : 2}
+}
+let b = e.c.c1 + a(1,2) * c + d[1] * 'a' - e.b(1,2) + (function d(){return 1})();
+  
