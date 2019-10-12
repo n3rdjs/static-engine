f12f81e67ab69e44036e94b60d69711effc3b447
ohk990102 <ohk990102@naver.com>
2019-10-12 15:51:00 +0900

    [Add] server feature enhanced

---
+++ server/analyze.js
@@ -0,0 +1,60 @@
+/* eslint-disable global-require */
+/* eslint-disable no-console */
+const esprima = require('esprima');
+const walkes = require('walkes');
+const esgraph = require('../esgraph');
+const { staticEngine } = require('../engine');
+
+function analyze(code, options) {
+  if(require.cache[require.resolve('../esgraph')]) {
+    delete require.cache[require.resolve('../esgraph')];
+  }
+  
+  console.log(1);
+  console.log(code);
+  let text = '';
+  try {
+    const fullAst = esprima.parse(code, { range: true });
+    const functions = findFunctions(fullAst);
+
+    text += 'digraph cfg {';
+    text += 'node [shape="box"]';
+    const dotOptions = { counter: 0, source: code };
+    functions.concat(fullAst).forEach((ast, i) => {
+      let cfg;
+      let label = '[[main]]';
+      if (ast.type.includes('Function')) {
+        cfg = esgraph(ast.body);
+        const name = (ast.id && ast.id.name) || '';
+        const params = ast.params.map(p => p.name);
+        label = `function ${name}(${params})`;
+      } else {
+        cfg = esgraph(ast);
+      }
+
+      text += `subgraph cluster_${i}{`;
+      text += `label = "${label}"`;
+      text += esgraph.dot(cfg, dotOptions);
+      text += '}';
+    });
+    text += '}';
+  } catch (e) {
+    console.log(e);
+    return { success: false, message: e.message };
+  }
+  return { success: true, cfg: text };
+}
+
+function findFunctions(ast) {
+  const functions = [];
+  function handleFunction(node, recurse) {
+    functions.push(node);
+    recurse(node.body);
+  }
+  walkes(ast, {
+    FunctionDeclaration: handleFunction,
+    FunctionExpression: handleFunction,
+  });
+  return functions;
+}
+module.exports = analyze;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { staticEngine } = require('./engine');
+const { staticEngine } = require('../engine');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
+
+const analyze = require('./analyze');
 const app = express();
 
 app.engine('html', require('ejs').renderFile);
@@ -11,9 +14,16 @@ app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')))
+app.use('/style', express.static(path.join(__dirname, 'style')))
+
 app.get('', (req, res) => {
 	res.render('index');
 });
+
+app.get('/view', (req, res) => {
+	res.render('view');
+})
 
 app.post('/test', (req, res) => {
 	var source = req.body.source || '';
@@ -21,5 +31,9 @@ app.post('/test', (req, res) => {
 	console.log(result);
 	res.render('result', { result: result });
 });
+
+app.post('/analyze', (req, res) => {
+	res.send(analyze(req.body.code));
+})
 
 app.listen(8080);
