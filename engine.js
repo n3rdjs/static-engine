ce2d86beaf90cddee542a436310a081dd238415b
posix <posix.lee@gmail.com>
2019-11-06 07:13:16 +0900

    1106

--- engine.js
+++ engine.js
@@ -10,12 +10,12 @@ class scope {
         this.range = range;
         this.type = type;
         this.child = [];
-        this.variables = [];
+        this.variabless = [];
         this.functions = [];
     }
 }
 
-class variable_info {
+class variables_info {
     constructor(name, scope, type, value, argument, range) {
         this.type = type;
         this.name = name;
@@ -47,18 +47,19 @@ class StaticEngine {
 
         this.real_func_names = [];
         this.real_func_call = [];
-        this.real_variable = [];
+        this.real_variables = [];
         this.real_func_scope = [];
 
         this.parent_scope_range;
         this.scope_array = {};
 
-        this.variable = [];
-        this.entire_function = [];
+        this.variables = [];
+        this.functions = [];
 
         if (!!options.debug) {
             log = console.log;
         }
+
         if (!!options.customlog) {
             log = options.customlog;
         }
@@ -71,54 +72,50 @@ class StaticEngine {
             });
             var cfg = esgraph(ast);
 
-            fs.writeFileSync('AST.json', JSON.stringify(ast, null, 4));
+            // don't need this anymore. just for debug
+            // fs.writeFileSync(path.join(__dirname, 'out', 'ast.json'), JSON.stringify(ast, null, 4));
 
             var self = this;
-            self.traverse(ast, function (node, parent_node) {
+            self.traverse(ast, (node, parent_node) => {
                 if (node.type) {
                     self.make_scope(node, parent_node);
                 }
             }, ast);
 
-            self.traverse(ast, function (node, parent_node) {
+            self.traverse(ast, (node, parent_node) => {
                 if (node.type) {
-                    self.get_variable_declaration(node, parent_node);
+                    self.get_variables_declaration(node, parent_node);
                     self.get_function(node, parent_node);
                 }
             }, ast);
 
             this.get_parameter();
 
-            self.traverse(ast, function (node, parent_node) {
-                if (node.type) { //range -> undefined
-                    self.get_variable_assignment(node, parent_node);
+            self.traverse(ast, (node, parent_node) => {
+                if (node.type) {
+                    self.get_variables_assignment(node, parent_node);
                 }
             }, ast);
 
-            let variable_num = 0;
-            for (let i of this.variable) {
-                variable_num++;
-                log(chalk.bgRed(` Variable Info (${variable_num}) `));
-                Object.keys(i).forEach(item => {
-                    log(item, ":", i[item]);
-                })
-                log("");
-            }
-            let function_num = 0;
-            for (let i of this.entire_function) {
-                function_num++;
-                log(chalk.bgBlue(` Function Info (${function_num}) `));
-                Object.keys(i).forEach(item => {
-                    if (item != 'parent') {
-                        log(item, ":", i[item]);
-                    }
-                })
-                log("");
-            }
+            this.variables.forEach((variables, index) => {
+                log(chalk.bgRed(`        Variable Info (${index + 1})        `));
+                for (let key of Object.keys(variables)) {
+                    log(key.padStart(8), ':', variables[key]);
+                }
+                log();
+            });
+
+            this.functions.forEach((func, index) => {
+                log(chalk.bgBlue(`        Function Info (${index + 1})        `));
+                for (let key of Object.keys(func)) {
+                    log(key, ':', func[key]);
+                }
+                log();
+            });
 
             var result = {
-                'variable': this.variable,
-                'function': this.entire_function,
+                'variables': this.variables,
+                'functions': this.functions,
                 'ast': ast,
                 'cfg': cfg
             }
@@ -132,9 +129,7 @@ class StaticEngine {
     }
 
     isFlowNode(astnode) {
-        if (astnode.cfg) {
-            return true;
-        } else return false;
+        return !!astnode.cfg;
     }
 
     get_parameter() {
@@ -146,24 +141,24 @@ class StaticEngine {
             range: [],
             argument: []
         };
-        for (let i of this.entire_function) {
-            for (let j in i.argument) {
-                if (i.argument[j].type == 'Identifier') {
+        this.functions.forEach(node => {
+            Object.keys(node.argument).forEach(key => {
+                if (node.argument[key].type == 'Identifier') {
                     data.type = 'function parameter';
-                    data.name = i.argument[j].name;
-                    data.range = i.argument[j].range;
+                    data.name = node.argument[key].name;
+                    data.range = node.argument[key].range;
 
                     data.scope = this.find_scope(this.scope_array[this.parent_scope_range], data.range, this.scope_array[this.parent_scope_range], 'function');
                     data.range = [];
 
-                    this.variable.push(new variable_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
-                    this.scope_array[data.scope.range].variables.push(new variable_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
+                    this.variables.push(new variables_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
+                    this.scope_array[data.scope.range].variabless.push(new variables_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
                 }
-            }
-        }
+            });
+        })
     }
 
-    get_variable_declaration(node) {
+    get_variables_declaration(node) {
         let data = {
             name: '',
             type: '',
@@ -212,11 +207,11 @@ class StaticEngine {
             if (data.type == 'var') data.scope = this.find_scope(this.scope_array[this.parent_scope_range], node.range, this.scope_array[this.parent_scope_range], 'function');
             else data.scope = this.find_scope(this.scope_array[this.parent_scope_range], node.range, this.scope_array[this.parent_scope_range], 'block');
 
-            this.variable.push(new variable_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
-            this.scope_array[data.scope.range].variables.push(new variable_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
+            this.variables.push(new variables_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
+            this.scope_array[data.scope.range].variabless.push(new variables_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
         }
     }
-    get_variable_assignment(node) {
+    get_variables_assignment(node) {
         let data = {
             name: '',
             type: '',
@@ -232,17 +227,16 @@ class StaticEngine {
 
                 // if not??
             }
-            // find if same identifier name is defined
-            let found = this.var_if_declared(node);
-            if (found == 0) {
+            // check variable already declared
+            if (!this.is_declared(node)) {
                 // if doesn't exist
                 data.type = 'global';
                 data.name = node.left.name; //a2
                 data.scope = this.scope_array[this.parent_scope_range]
                 data.value = node.right.type;
                 data.range = []; //node.right.range;
-                this.variable.push(new variable_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
-                this.scope_array[data.scope.range].variables.push(new variable_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
+                this.variables.push(new variables_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
+                this.scope_array[data.scope.range].variabless.push(new variables_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
             }
         }
     }
@@ -269,15 +263,15 @@ class StaticEngine {
             }
             if (parent_node.type == 'CallExpression') {
                 if (node.id) data.name = node.id.name;
-                else data.name = node.id;
+                else data.name = node.id; // what does it mean?
                 data.scope = node.range;
             } else {
                 if (node.id) data.name = node.id.name;
-                else data.name = node.id;
+                else data.name = node.id; // what does it mean?
                 data.scope = found_scope.range;
             }
             if (node.type == 'ArrowFunctionExpression' || node.type == 'FunctionExpression') {
-                this.variable.filter((item) => {
+                this.variables.filter((item) => {
                     if ((node.type == item.value) && (node.range == item.range)) {
                         data.function_type = item.type;
                         data.name = item.name;
@@ -292,7 +286,7 @@ class StaticEngine {
 
             var tmp_function = new function_info(data.name, data.scope, data.range, data.type, data.argument, data.parent, data.method_type, data.function_type);
             this.scope_array[data.scope].functions.push(tmp_function);
-            this.entire_function.push(tmp_function);
+            this.functions.push(tmp_function);
         }
 
     }
@@ -306,20 +300,22 @@ class StaticEngine {
                 return (this.find_scope(children, target_range, last_function_scope, scope_type));
             }
         }
-        if (scope_type == 'block') return parent_scope;
-        else if (scope_type == 'function') return last_function_scope;
+        if (scope_type == 'block') {
+            return parent_scope;
+        } else if (scope_type == 'function') {
+            return last_function_scope;
+        }
     }
 
-    var_if_declared(node) {
-        let found = 0;
-        for (let i of this.variable) {
+    is_declared(node) {
+        this.variables.forEach(i => {
             if (i.name == node.left.name) {
                 if (node.range[0] >= i.scope[0] && i.scope[1] >= node.range[1]) {
-                    found = i;
+                    return true;
                 }
             }
-        }
-        return found;
+        });
+        return false;
     }
 
     var_hoisting_fromdeclaration(node) {
@@ -336,13 +332,13 @@ class StaticEngine {
 
         let root = this.scope_array[this.parent_scope_range];
 
-        for (let i of root.variables) {
+        for (let i of root.variabless) {
             if (i.name == node2.id.name && i.type == 'global') {
 
                 if (data.scope.range[0] <= i.range[0] && i.range[1] <= data.scope.range[1]) {
                     i.type = node.kind;
 
-                    root.variables = root.variables.filter(function (value) {
+                    root.variabless = root.variabless.filter(function (value) {
                         return value != i;
                     });
 
@@ -358,13 +354,13 @@ class StaticEngine {
                     }
 
                     i.scope = data.scope.range;
-                    for (let j of this.variable) { // assume no duplicates
+                    this.variables.forEach(j => { // assume no duplicates
                         if (j.name == node2.id.name && j.type == 'global') {
                             j.type = node.kind;
                             j.scope = data.scope.range;
                         }
-                    }
-                    this.scope_array[data.scope.range].variables.push(new variable_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
+                    });
+                    this.scope_array[data.scope.range].variabless.push(new variables_info(data.name, data.scope.range, data.type, data.value, data.argument, data.range));
                     return data;
                 }
 
@@ -374,64 +370,61 @@ class StaticEngine {
     }
 
     make_scope(node, parent_node) {
-        var scope_flag = false;
-        var target_range;
-        var target_type;
-        if (node.type == 'BlockStatement') {
-            target_range = node.range;
-            scope_flag = true;
-            if (parent_node.type == 'ArrowFunctionExpression' || parent_node.type == 'FunctionDeclaration' || parent_node.type == 'FunctionExpression') {
-                target_type = 2;
-            } else target_type = 1;
+        var target_range, target_type, _flag = false;
+
+        switch(node.type) {
+            case 'BlockStatement':
+            case 'ForStatement':
+            case 'ForOfStatement':
+            case 'ForInStatement':
+                target_range = node.range;
+                _flag = true;
+                target_type = 1;
+
+                if (['ArrowFunctionExpression', 'FunctionDeclaration', 'FunctionExpression'].includes(parent_node.type)) {
+                    target_type = 2;
+                }
+                break;
+
+            case 'ArrowFunctionExpression':
+            case 'FunctionDeclaration':
+            case 'FunctionExpression':
+                if (node.body.type == 'BlockStatement') {
+                    target_range = node.range;
+                    _flag = true;
+                    target_type = 3;
+                } else if (parent_node.type == 'CallExpression') {
+                    target_range = node.range;
+                    _flag = true;
+                    target_type = 4;
+                }
+                break;
+
+            case 'ClassBody':
+                target_range = node.range;
+                _flag = true;
+                if (parent_node.type == 'ClassDeclaration') {
+                    target_type = 4;
+                }
+                break;
+
+            case 'Program':
+                target_range = node.range;
+                target_type = 0;
+                this.parent_scope_range = target_range;
+                _flag = true;
+                break;
+
         }
 
-        if (node.type == 'ArrowFunctionExpression' || node.type == 'FunctionDeclaration' || node.type == 'FunctionExpression') {
-            if (node.body.type == 'BlockStatement') {
-                target_range = node.range;
-                scope_flag = true;
-                target_type = 3;
-            }
-            if (parent_node.type == 'CallExpression') {
-                target_range = node.range;
-                scope_flag = true;
-                target_type = 4;
-            }
-        }
-
-        if (node.type == 'ClassBody') {
-            target_range = node.range;
-            scope_flag = true;
-            if (parent_node.type == 'ClassDeclaration') target_type = 4;
-        }
-        if (node.type == "Program") {
-            target_range = node.range;
-            target_type = 0;
-            this.parent_scope_range = target_range;
-            scope_flag = true;
-        }
-        if (node.type == 'ForStatement') {
-            target_range = node.range;
-            target_type = 1; //same as block
-            scope_flag = true;
-        }
-        if (node.type == 'ForOfStatement') {
-            target_range = node.range;
-            target_type = 1; //same as block
-            scope_flag = true;
-        }
-        if (node.type == 'ForInStatement') {
-            target_range = node.range;
-            target_type = 1; //same as block
-            scope_flag = true;
-        }
-        if (scope_flag == true && !this.scope_array[target_range]) {
+        if (_flag && !this.scope_array[target_range]) {
             this.scope_array[target_range] = (new scope(target_range, target_type));
             if (target_type != 0) this.push_scope(this.scope_array[this.parent_scope_range], target_range);
         }
     }
     push_scope(parent_scope, target_range) {
         if (parent_scope.child.length == 0) {
-            parent_scope.child.push(this.scope_array[target_range]);
+            parent_scope.child = [this.scope_array[target_range]];
             return;
         }
         for (let children of parent_scope.child) {
@@ -448,12 +441,12 @@ class StaticEngine {
         if (!node || typeof node != 'object') return;
         func(node, parent_node);
 
-        for (let key of Object.keys(node)) {
+        Object.keys(node).forEach(key => {
 
             let child = node[key];
             this.traverse(child, func, node);
 
-        }
+        });
     }
 }
 
