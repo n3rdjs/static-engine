a774483fb79519e41ef62836b4e70faa5408eaf8
ohk990102 <ohk990102@naver.com>
2019-11-29 10:41:44 +0900

    [Add] checkASTPattern and Dominator
     - function checkASTPattern(astNode, pattern)
    checks whether given astNode matches pattern
    
     - class Dominator
    generate dominator tree from cfg

--- analyzer.js
+++ analyzer.js
@@ -1,20 +1,17 @@
-function traverseAST(node, func) {
-	func(node);
-	for (let key in node) {
-        if(node.hasOwnProperty(key)) {
-            let child = node[key];
-            if (typeof child === 'object' && child !== null) {
-                if (Array.isArray(child)) {
-                    child.forEach(function (node) {
-                        traverseAST(node, func);
-                    });
+function traverseAST(node, options, callback) {
+    const next = ()=> {
+        Object.keys(node).forEach((key) => {
+            if (key === 'cfg') return;
+            if (Array.isArray(node[key])) {
+                for(const childNode of node[key]) {
+                    traverseAST(childNode, options, callback);
                 }
-                else {
-                    traverseAST(child, func);
-                }
+            } else {
+                traverseAST(node[key], options, callback);
             }
-        }
-    }
+        })
+    };
+    callback(node, next);
 }
 
 
@@ -80,6 +77,155 @@ function findCFGByEntryASTNode(entryASTNode, cfg) {
         }
     }
     return null;
+}
+
+class Dominator {
+    constructor(cfg) {
+        this.cfg = cfg;
+        this.nodes = [undefined, ...this.cfg[2]];
+        this.node_num = new Map();
+        this.n = this.nodes.length; // this.cfg.length + 1
+        for (let i = 1; i < this.n; i++) {
+            this.node_num.set(this.nodes[i], i);
+        }
+        this.r = this.node_to_num(this.cfg[0]);
+        this.parent = new Array(this.n);
+        this.ancestor = new Array(this.n);
+        this.child = new Array(this.n);
+        this.vertex = new Array(this.n);
+        this.label = new Array(this.n);
+        this.semi = new Array(this.n);
+        this.size = new Array(this.n);
+        this.pred = new Array(this.n);
+        this.bucket = new Array(this.n);
+        this.dom = new Array(this.n);
+
+    }
+
+    compute() {
+        for (let v = 1; v < this.n; v++) {
+            this.pred[v] = new Set();
+            this.bucket[v] = new Set();
+            this.semi[v] = 0;
+        }
+        this.n = 0;
+        this.dfs(this.r);    // should be this.dfs(1);
+        this.size[0] = this.label[0] = this.semi[0] = 0;
+        for (let i = this.n; i >= 2; i--) {
+            let w = this.vertex[i];
+            for (const v of this.pred[w]) {
+                let u = this.eval(v);
+                if (this.semi[u] < this.semi[w]) {
+                    this.semi[w] = this.semi[u];
+                }
+            }
+            this.bucket[this.vertex[this.semi[w]]].add(w);
+            this.link(this.parent[w], w);
+            for (const v of this.bucket[this.parent[w]]) {
+                this.bucket[this.parent[w]].delete(v);
+                let u = this.eval(v);
+                if (this.semi[u] < this.semi[v]) {
+                    this.dom[v] = u;
+                }
+                else {
+                    this.dom[v] = this.parent[w];
+                }
+            }
+        }
+
+        for (let i = 2; i < this.n; i++) {
+            let w = this.vertex[i];
+            if (this.dom[w] !== this.vertex[this.semi[w]]) {
+                this.dom[w] = this.dom[this.dom[w]];
+            }
+        }
+        this.dom[this.r] = 0;
+    }
+
+    num_to_node(num) {
+        if (num < 1 || num >= this.nodes.length) {
+            throw "num_to_node error";
+        }
+        return this.nodes[num];
+    }
+
+    node_to_num(node) {
+        if (!this.node_num.has(node)) {
+            throw "node_to_num error";
+        }
+        return this.node_num.get(node);
+    }
+
+    succ(v) {
+        const result = [];
+        for (const key of ['normal', 'true', 'false', 'exception']) {
+            const succ = this.num_to_node(v)[key];
+            if (succ !== undefined) {
+                result.push(this.node_to_num(succ));
+            }
+        }
+        return result;
+    }
+
+    dfs(v) {
+        this.n += 1;
+        this.semi[v] = this.n;
+        this.vertex[this.n] = this.label[v] = v;
+        this.ancestor[v] = this.child[v] = 0;
+        this.size[v] = 1;
+
+        for (const w of this.succ(v)) {
+            if (this.semi[w] === 0) {
+                this.parent[w] = v;
+                this.dfs(w);
+            }
+            this.pred[w].add(v);
+        }
+    }
+
+    compress(v) {
+        if (this.ancestor[this.ancestor[v]] !== 0) {
+            this.compress(this.ancestor[v]);
+            if (this.semi[this.label[this.ancestor[v]]] < this.semi[this.label[v]]) {
+                this.label[v] = this.label[this.ancestor[v]];
+            }
+            this.ancestor[v] = this.ancestor[this.ancestor[v]];
+        }
+    }
+
+    eval(v) {
+        if (this.ancestor[v] === 0) {
+            return this.label[v];
+        }   
+        this.compress(v);
+        if (this.semi[this.label[this.ancestor[v]]] >= this.semi[this.label[v]]) {
+            return this.label[v];
+        }
+        return this.label[this.ancestor[v]];
+    }
+
+    link(v, w) {
+        let s = w;
+        while (this.semi[this.label[w]] < this.semi[this.label[this.child[s]]]) {
+            if (this.size[s] + this.size[this.child[this.child[s]]] >= 2) {
+                this.ancestor[this.child[s]] = s;
+                this.child[s] = this.child[this.child[s]];
+            }
+            else {
+                this.size[this.child[s]] = this.size[s];
+                s = this.ancestor[s] = this.child[s];
+            }
+        }
+        this.label[s] = this.label[w];
+        this.size[v] = this.size[v] + this.size[w];
+        if (this.size[v] < 2 * this.size[w]) {
+            s, this.child[v] = this.child[v], s;
+        }
+        while (s !== 0) {
+            this.ancestor[s] = v;
+            s = this.child[s];
+        }
+    }
 }
 
 function scc(ast, cfg){
@@ -225,5 +371,6 @@ module.exports = {
     traverseCFG: traverseCFG,
     findFunctionInfoByCallExpression: findFunctionInfoByCallExpression,
     findCFGByEntryASTNode: findCFGByEntryASTNode,
-    checkASTPattern: checkASTPattern
+    checkASTPattern: checkASTPattern,
+    Dominator: Dominator
 }
