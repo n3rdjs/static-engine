8f97ef4e67868fb330dd687bc5a9db4a503b31e3
time4ruin <time4_ruin@naver.com>
2019-12-18 23:01:43 +0900

    add function declaration to variables

--- input_folder/tmp.js
+++ input_folder/tmp.js
@@ -1,38 +1,6 @@
-function set(object, path, value) {
-  if (!isObj(object) || typeof path !== 'string') {
-    return object;
-  }
-
-  const root = object;
-  const pathArray = getPathSegments(path);
-
-  for (let i = 0; i < pathArray.length; i++) {
-    const p = pathArray[i];
-
-    if (!isObj(object[p])) {
-      object[p] = {};
-    }
-
-    if (i === pathArray.length - 1) {
-      object[p] = value;
-    }
-
-    object = object[p];
-  }
-
-  p = 0;
-  object = object[p];
-  return root;
-}
-
-function a(){
-  let n = 0;
-  while (n<1){
-    n++;
-  }
+function a(x){
   return 1;
 }
-
-function b(){
-  return 1 + 2;
+var b = function(x){
+  return 2;
 }
